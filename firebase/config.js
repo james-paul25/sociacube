de5c601a9be387b1218af6739c2346eb8e68e853
James Paul <dacaldacaljamespaul@gmail.com>
2025-06-25 13:22:06 +0800

    update

--- firebase/config.js
+++ firebase/config.js
@@ -1,18 +1,18 @@
 import { initializeApp, getApps, getApp } from "firebase/app";
-import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  authDomain: "sociacube.firebaseapp.com",
-  projectId: "sociacube",
-  storageBucket: "sociacube.appspot.com",
-  messagingSenderId: "129358505674",
-  appId: "1:129358505674:web:8fd422e36b13a5ca773633",
-  measurementId: "G-QWXQF5T4WZ"
+  apiKey: process.env.EXPO_PUBLIC_FIREBASE_API_KEY,
+  projectId: process.env.EXPO_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.EXPO_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  appId: process.env.EXPO_PUBLIC_FIREBASE_APP_ID,
+  messagingSenderId: process.env.EXPO_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  measurementId: process.env.EXPO_PUBLIC_FIREBASE_MEASUREMENT_ID,
+  authDomain: process.env.EXPO_PUBLIC_FIREBASE_AUTH_DOMAIN 
 };
 
 const app = getApps().length === 0 ? initializeApp(firebaseConfig) : getApp();
-const auth = getAuth(app);
+
 const db = getFirestore(app);
 
-export { auth, db };
+export { db };
